208b5cdeb11af1c2024e74d84d0b3b990bb1827e
Debraj1723 <debraj.basak23@gmail.com>
2025-12-01 14:40:16 +0530

    show connection request

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,9 +1,69 @@
-const Requests = () =>{
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import constants from "../utils/constant";
+import { addRequests } from "../utils/requestStore";
+import { useEffect } from "react";
+
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+  const dispatch = useDispatch();
+  const fetchRequests = async () => {
+    const res = await axios.get(constants.baseUrl + "users/requests", {
+      withCredentials: true,
+    });
+    dispatch(addRequests(res.data));
+  };
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return;
+
+  if (requests.length === 0) {
     return (
-        <div>
-            <h1>Requests Page</h1>
-        </div>
-    )
-}
+      <div className="flex justify-center my-10">
+        <h1 className="text-bold text-2xl">No Requests Found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Requests</h1>
+
+      {requests.map((request) => {
+        const { firstName, lastName, photoUrl, age, gender, about } =
+          request.fromUserID;
+
+        return (
+          <div
+            className="flex justify-between items-center m-5 p-4 rounded-lg bg-base-300 w-2/3 mx-auto"
+            key={request._id}
+          >
+            <div>
+              <img
+                alt="photo"
+                className="w-20 h-20 rounded-full"
+                src={photoUrl}
+              />
+            </div>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">
+                {firstName + " " + lastName}
+              </h2>
+              {age && gender && <p>{age + ", " + gender}</p>}
+              <p>{about}</p>
+            </div>
+            <div>
+              <button className="btn btn-primary mx-2">Reject</button>
+              <button className="btn btn-secondary mx-2">Accept</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Requests;
